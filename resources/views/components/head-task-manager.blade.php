046191f43840ee1ba0659f1385b6dae99764e79b
Mikail <miklsuhanov@gmail.com>
2023-10-04 12:11:13 +1000

    uppdate head-task-manager

--- resources/views/components/head-task-manager.blade.php
+++ resources/views/components/head-task-manager.blade.php
@@ -1,7 +1,10 @@
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="csrf-param" content="_token" />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="preload" as="style" href="//localhost:8000/build/assets/app-bd32b7f6.css">
+    <link rel="modulepreload" href="//localhost:8000/build/assets/app-0318cbde.js">
+    <link rel="stylesheet" href="//localhost:8000/build/assets/app-bd32b7f6.css">
+    <script type="module" src="//localhost:8000/build/assets/app-0318cbde.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
