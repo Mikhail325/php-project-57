a90c13b1e91f647fc41f565460d14f9fa72e3037
Mikail <miklsuhanov@gmail.com>
2023-10-10 22:20:58 +1000

    uppdate Makefile

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,6 +4,6 @@
 <h1 class="mt-5 mb-5">Привет от Хекслета!</h1>
 <div>
   <h2>Это простой менеджер задач на Laravel</h2>
-  <a class="btn btn-success" href="{{route('tasks.index')}}" role="button">Нажми меня</a>
+  <a class="btn btn-primary" href="{{route('tasks.index')}}" role="button">Нажми меня</a>
 </div>
 @endsection
